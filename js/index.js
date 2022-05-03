bf6b6deec491bd36b4525710746a04e6ab6a9e9a
Annemarie <a.lucernoni0@gmail.com>
2022-05-03 10:25:58 -0700

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {});
+
+fetch('http://localhost:3000/books')
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(function(element) {
+            document.createElement('li')
+            document.getElementById('list').append('li')
+        })
+    })
+
+let turnBookIntoLi = book => {
+    let bookLi = document.createElement('li')
+    bookLi.textContent = book.title
+}
